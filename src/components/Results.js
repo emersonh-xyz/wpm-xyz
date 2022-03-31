efd963a9af1e0ecfd1b4fb3a7c60def5e083cbe3
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2022-03-31 08:12:38 -0400

    State data sending

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,27 +1,33 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 
-export default function Results(){
-    return(
+
+export default function Results({correctState}) {
+    
+    
+    return (
         <div className="section">
 
 
-        <div className="section is-size-2 has-text-centered" >
-            Results
+            <div className="section is-size-2 has-text-centered" >
+                Your results: 
+            </div>
+            <div className="columns">
+                <div className="column has-text-centered">
+                    <p className="is-size-3">WPM:</p>
+                    <p className="has-text-primary is-size-1">
+                        50
+                    </p>
+                </div>
+                <div className="column has-text-centered">
+                    <p className="is-size-3">Accuracy:</p>
+                    <p className="has-text-info is-size-1">
+                        {correctState.correct}
+                    </p>
+                </div>
+            </div>
+            <div className="section">
+            <div className="button is-fullwidth is-primary">Play again</div>
+            </div>
         </div>
-        <div className="columns">
-          <div className="column has-text-centered">
-            <p className="is-size-3">WPM:</p>
-            <p className="has-text-primary is-size-1">
-              50
-            </p>
-          </div>
-          <div className="column has-text-centered">
-            <p className="is-size-3">Accuracy:</p>
-              <p className="has-text-info is-size-1">
-                100%
-              </p>
-          </div>
-        </div>
-      </div>
     )
 }
